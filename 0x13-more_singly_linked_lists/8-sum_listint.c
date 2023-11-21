2791ade1552f42eb6f39ee78ca824977912df3ed
Psumtin <jegedeog@gmail.com>
2023-11-21 13:27:46 -0800

    8-sum_listint.c

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -5,18 +5,17 @@
  * @head: first node in the linked list
  *
  * Return: resulting sum
- */
+*/
 int sum_listint(listint_t *head)
 {
- int sum = 0;
- listint_t *temp = head;
+int sum = 0;
+listint_t *temp = head;
 
- while (temp)
- {
- sum += temp->n;
- temp = temp->next;
- }
-
- return (sum);
+while (temp)
+{
+sum += temp->n;
+temp = temp->next;
 }
 
+return (sum);
+}
